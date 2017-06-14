3e21484d9307a40edb2282970f2ee9d9528f9246
tiant1 <tian_tian1@outlook.com>
2017-06-14 10:56:37 +0800

    1056

--- exceptions_raise.py
+++ exceptions_raise.py
@@ -13,7 +13,7 @@ try:
 except EOFError:
     print('Why did you do an EOF on me?')
 except ShortInputException as ex:
-    print(('ShortInputException: The input was ' + \
+    print(('ShortInputException: The input was ' +
            '{0} long, expected at least {1}').format(ex.length, ex.atleast))
 else:
     print('No exception was raised.')
